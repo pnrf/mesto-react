cfeba8df32a338fac9d43da422fbf6e34f87ef79
Pankratov <pnrf@yandex.ru>
2022-06-14 17:12:50 +0300

    build: add markup to ImagePopup.js

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,5 +1,13 @@
 function ImagePopup() {
-  return;
+  return (
+    <section className="popup popup_type_image">
+      <figure className="popup__container popup__container_type_image">
+        <button className="popup__close-button popup__close-button_type_image" type="button"></button>
+        <img src="#" alt="" className="popup__image" />
+        <figcaption className="popup__figcaption"></figcaption>
+      </figure>
+    </section>
+  );
 };
 
 export default ImagePopup;
