12599895dc322236a5c6b1142d2f6c2318f444ee
Pankratov <pnrf@yandex.ru>
2022-06-14 06:42:55 +0300

    feat: add handlers to buttons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,42 @@
 function Main() {
-    return (
-        <main>
-        <section className="profile">
-          <div className="profile__container">
-            <button className="profile__avatar-edit-button" type="button"><img src="<%=require('./images/img-profile-avatar.jpg')%>" alt="Аватар для профайла" className="profile__avatar" /></button>
-            <div className="profile__describe">
-              <div className="profile__title-wrapper">
-                <h1 className="profile__title">Жак-Ив Кусто</h1>
-                <button className="profile__edit-button" type="button"></button>
-              </div>
-              <p className="profile__subtitle">Исследователь океана</p>
+  function handleEditAvatarClick(event) {
+    event.target.addEventListener('click', () => {
+      document.querySelector('.popup_type_avatar').classList.add('popup_opened');
+    });
+  };
+
+  function handleEditProfileClick(event) {
+    event.target.addEventListener('click', () => {
+      document.querySelector('.popup_type_profile').classList.add('popup_opened');
+    });
+  };
+
+  function handleAddPlaceClick(event) {
+    event.target.addEventListener('click', () => {
+      document.querySelector('.popup_type_cards').classList.add('popup_opened');
+    });
+  };
+
+  return (
+    <main>
+      <section className="profile">
+        <div className="profile__container">
+          <button onClick={handleEditAvatarClick} className="profile__avatar-edit-button" type="button"><img src="<%=require('./images/img-profile-avatar.jpg')%>" alt="Аватар для профайла" className="profile__avatar" /></button>
+          <div className="profile__describe">
+            <div className="profile__title-wrapper">
+              <h1 className="profile__title">Жак-Ив Кусто</h1>
+              <button onClick={handleEditProfileClick} className="profile__edit-button" type="button"></button>
             </div>
+            <p className="profile__subtitle">Исследователь океана</p>
           </div>
-          <button className="profile__add-button" type="button"></button>
-        </section>
-        <section className="cards">
-          <ul className="cards__list"></ul>
-        </section>
-      </main>
-    )
+        </div>
+        <button onClick={handleAddPlaceClick} className="profile__add-button" type="button"></button>
+      </section>
+      <section className="cards">
+        <ul className="cards__list"></ul>
+      </section>
+    </main>
+  )
 };
 
 export default Main;
