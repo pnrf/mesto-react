b8b7ec62db1b47ac9522a5519f63d5fc78410621
Pankratov <pnrf@yandex.ru>
2022-06-14 21:19:39 +0300

    feat: port API module for profile data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,37 @@
+import React, { useEffect } from 'react';
+import api from '../utils/api';
+import initialAvatar from '../images/img-profile-avatar.jpg';
+
 function Main(props) {
+  const [userName, setUserName] = React.useState('Жак-Ив Кусто')
+  const [userDescription, setUserDescription] = React.useState('Исследователь океана')
+  const [userAvatar, setUserAvatar] = React.useState(initialAvatar)
+
+
+  useEffect(() => {
+    Promise.all([api.getUserInfo()]).then(([profileInfo]) => {
+      console.log("FFF", profileInfo.avatar);
+      setUserName(profileInfo.name)
+      setUserDescription(profileInfo.about)
+      setUserAvatar(profileInfo.avatar)
+    }).catch((err) => {
+      console.error(err);
+    })
+  }, [])
+
   return (
     <main>
       <section className="profile">
         <div className="profile__container">
-          <button onClick={props.onEditAvatar} className="profile__avatar-edit-button" type="button"><img src="<%=require('./images/img-profile-avatar.jpg')%>" alt="Аватар для профайла" className="profile__avatar" /></button>
+          <button onClick={props.onEditAvatar} style={{ backgroundImage: `url(${userAvatar})` }} className="profile__avatar-edit-button" type="button">
+            {/* <img src={userAvatar} alt="Аватар для профайла" className="profile__avatar" /> */}
+          </button>
           <div className="profile__describe">
             <div className="profile__title-wrapper">
-              <h1 className="profile__title">Жак-Ив Кусто</h1>
+              <h1 className="profile__title">{userName}</h1>
               <button onClick={props.onEditProfile} className="profile__edit-button" type="button"></button>
             </div>
-            <p className="profile__subtitle">Исследователь океана</p>
+            <p className="profile__subtitle">{userDescription}</p>
           </div>
         </div>
         <button onClick={props.onAddPlace} className="profile__add-button" type="button"></button>
